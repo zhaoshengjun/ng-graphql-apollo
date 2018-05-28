9341e3f0b8ce325d2737b83933e1ba654deaa6b6
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-28 22:10:54 +1000

    basic server setup

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
+});
