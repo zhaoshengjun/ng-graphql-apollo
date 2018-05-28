ea48fbe749f8cdcacd46734171ff628073b141b8
Sam Zhao <zhao.shengjun@gmail.com>
2018-05-28 22:17:25 +1000

    setup graphql

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { graphqlExpress } from "apollo-server-express";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-});
+app.use("/graphql", bodyParser.json(), graphqlExpress({}));
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
